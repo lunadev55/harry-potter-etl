163b08fdb386e39d01053a18e97e3e2243d63882
Andre Luna <alm3@cin.ufpe.br>
2025-02-22 01:12:08 -0300

    Adds db contexts, connection string and data stuff

---
+++ ETL/ETL.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ETL.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETL.Persistence.Contexts;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+}
